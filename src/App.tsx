b4833647cc26508991e14eecec1089e567792a4e
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-03-18 20:57:14 -0300

    Setup react-router-dom with example page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Example from "./pages/Example"
+
 import "./assets/styles/globalStyles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pagina inicial</h1>
-    </div>
+    <Router>
+      <Route path="/" exact component={Example} />
+    </Router>
   );
 }
 
