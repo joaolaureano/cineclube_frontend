a463b142cdacdad876174f497a1d99a9f62d3099
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-03-30 01:03:00 -0300

    Update files to typescript

--- src/services/login.ts
+++ src/services/login.ts
@@ -9,23 +9,23 @@ export const logIn = () => {
     .auth()
     .signInWithPopup(provider)
     .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      const credential = result.credential;
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const token = credential.accessToken;
-      // The signed-in user info.
       const user = result.user;
-      console.log(token);
+
+      const token = user?.getIdToken;
+
       return token;
     })
     .catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
+
       // The email of the user's account used.
       const email = error.email;
+
       // The firebase.auth.AuthCredential type that was used.
       const credential = error.credential;
-      // ...
+
+      console.log(errorCode, errorMessage, email, credential);
     });
 };
