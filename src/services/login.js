0dade6abe9475d23b9c639b4dcac6877bc3730e3
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-03-30 00:06:42 -0300

    Update to use built-in dotenv package
    The other package (react-dotenv) was creating an env.js file inside the
    public folder with all the environment vars.

--- src/services/login.js
+++ src/services/login.js
@@ -1,37 +1,31 @@
 // import {firebase} from '../utils/firebase'
-import { firebaseAuth } from '../utils/firebase'
-import firebase from 'firebase';
-
-
-
-
+import { firebaseAuth } from "../utils/firebase";
+import firebase from "firebase";
 
 export const logIn = () => {
+  const provider = new firebaseAuth.GoogleAuthProvider();
 
-    var provider = new firebaseAuth.GoogleAuthProvider();
-
-    firebase.auth()
-        .signInWithPopup(provider)
-        .then((result) => {
-            /** @type {firebase.auth.OAuthCredential} */
-            var credential = result.credential;
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            var token = credential.accessToken;
-            // The signed-in user info.
-            var user = result.user;
-            console.log(token);
-            return token;
-        }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
-
-
-
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      const credential = result.credential;
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(token);
+      return token;
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = error.credential;
+      // ...
+    });
 };
