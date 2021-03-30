180eeed331bd914372de1d02880c0c6a39fdfad9
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-03-30 17:41:22 -0300

    Setup Material UI and add some examples

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,34 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const primary = "#e08314";
+const secondary = "#cccccc";
+const textPrimary = "#4d4d4d";
+const background = "#f2f2f2";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: primary,
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: secondary,
+      contrastText: textPrimary,
+    },
+    text: {
+      primary: textPrimary,
+    },
+    background: {
+      default: background,
+    },
+  },
+  typography: {
+    fontFamily: "Barlow",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
+export default theme;
