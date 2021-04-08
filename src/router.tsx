24194d88f0002e1ca1ef287bc9a2b5a0b4980d80
Csvop <54179429+Csvop@users.noreply.github.com>
2021-04-08 03:26:44 -0300

    Tela Home v1.0

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Example from "./pages/Example";
 import LoginTest from "./pages/LoginTest";
+import Home from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const Router = (): JSX.Element => {
@@ -11,6 +12,7 @@ const Router = (): JSX.Element => {
       <Switch>
         <Route path="/" exact component={Example} />
         <Route path="/logintest" exact component={LoginTest} />
+        <Route path="/home" exact component={Home} />
         <ProtectedRoute path="/example" exact component={Example} />
       </Switch>
     </BrowserRouter>
