d49ad2866f9fd83b45a111d2862056331abbe320
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-04-27 21:38:05 -0300

    Add menus icons

--- src/components/CustomIcon/index.tsx
+++ src/components/CustomIcon/index.tsx
@@ -4,9 +4,23 @@ import watched from "../../assets/images/icons/actions-watched.svg";
 import wantToWatch from "../../assets/images/icons/actions-want-to-watch.svg";
 import dontWantToWatch from "../../assets/images/icons/actions-dont-want-to-watch.svg";
 import undo from "../../assets/images/icons/actions-undo.svg";
+import menuWatched from "../../assets/images/icons/menu-watched.svg";
+import menuWantToWatch from "../../assets/images/icons/menu-want-to-watch.svg";
+import menusettings from "../../assets/images/icons/menu-settings.svg";
+import menuProfile from "../../assets/images/icons/menu-profile.svg";
+import menuAchievements from "../../assets/images/icons/menu-achievements.svg";
 
 interface CustomIconProps {
-  type: "watched" | "wantToWatch" | "dontWantToWatch" | "undo";
+  type:
+    | "watched"
+    | "wantToWatch"
+    | "dontWantToWatch"
+    | "undo"
+    | "menuWatched"
+    | "menuWantToWatch"
+    | "menusettings"
+    | "menuProfile"
+    | "menuAchievements";
   className?: string;
 }
 
@@ -20,6 +34,16 @@ const getIcon = (type: string) => {
       return dontWantToWatch;
     case "undo":
       return undo;
+    case "menuWatched":
+      return menuWatched;
+    case "menuProfile":
+      return menuProfile;
+    case "menuWantToWatch":
+      return menuWantToWatch;
+    case "menusettings":
+      return menusettings;
+    case "menuAchievements":
+      return menuAchievements;
   }
 };
 
