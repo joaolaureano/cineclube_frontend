e53917db61e5dfa222ba4058fe35c6c719a9d065
bruno.garcia99 <brunobreyergarcia@gmail.com>
2021-06-04 22:15:11 -0300

    getting random movie into the movie list

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -13,6 +13,8 @@ enum MovieUserStatus {
   DONT_WANT_TO_WATCH = "dont_want_to_watch",
 }
 
+const INTERVAL_BETWEEN_RANDOM = 3;
+
 const movies = {
   get: async (): Promise<AxiosResponse<MovieState>> => {
     const filters = JSON.parse(localStorage.getItem("filters") as string);
@@ -80,9 +82,85 @@ const composeMovieState = (data: string): MovieState => {
       movieIds.push(movie.id);
     });
   }
-
   const selectedMovieIndex = 0;
-  return { movies, movieIds, selectedMovieIndex };
+  return { movies, movieIds: reorderWithRandom(movieIds), selectedMovieIndex };
+};
+
+const getRndInteger = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const reorderWithRandom = (movieIds: number[]) => {
+  console.log(" ->" + movieIds.length);
+  let i = 0;
+
+  let newMovieIds = movieIds.slice(i, i + INTERVAL_BETWEEN_RANDOM - 1);
+  let randomMovieIndex = getRndInteger(
+    i + INTERVAL_BETWEEN_RANDOM,
+    movieIds.length - 1
+  );
+  let randomMovie = movieIds.slice(i + INTERVAL_BETWEEN_RANDOM)[
+    randomMovieIndex
+  ];
+  newMovieIds.push(randomMovie);
+  let oldMovieIds = [...newMovieIds];
+
+  let firstSlice = movieIds.slice(
+    i + INTERVAL_BETWEEN_RANDOM,
+    randomMovieIndex + i + INTERVAL_BETWEEN_RANDOM
+  );
+  // console.log("1")
+  // console.log(firstSlice)
+  let secondSlice = movieIds.slice(
+    randomMovieIndex + 1 + i + INTERVAL_BETWEEN_RANDOM
+  );
+  // console.log("2")
+  // console.log(secondSlice);
+
+  oldMovieIds = oldMovieIds.concat(firstSlice).concat(secondSlice);
+  i += INTERVAL_BETWEEN_RANDOM;
+  console.log(newMovieIds);
+  console.log(oldMovieIds);
+
+  while (i < movieIds.length) {
+    console.log(newMovieIds);
+    console.log(oldMovieIds);
+    newMovieIds = newMovieIds.concat(
+      oldMovieIds.slice(i, i + INTERVAL_BETWEEN_RANDOM - 1)
+    );
+    randomMovieIndex = getRndInteger(
+      i + INTERVAL_BETWEEN_RANDOM,
+      oldMovieIds.length - 1
+    );
+    randomMovie = oldMovieIds.slice(i + INTERVAL_BETWEEN_RANDOM)[
+      randomMovieIndex
+    ];
+    newMovieIds.push(randomMovie);
+    oldMovieIds = [...newMovieIds];
+
+    firstSlice = oldMovieIds.slice(
+      i + INTERVAL_BETWEEN_RANDOM,
+      randomMovieIndex + i + INTERVAL_BETWEEN_RANDOM
+    );
+    secondSlice = oldMovieIds.slice(
+      randomMovieIndex + 1 + i + INTERVAL_BETWEEN_RANDOM
+    );
+
+    i += INTERVAL_BETWEEN_RANDOM;
+
+    console.log(newMovieIds);
+    console.log(oldMovieIds);
+    // newMovieIds = oldMovieIds.slice(i, i+INTERVAL_BETWEEN_RANDOM);
+    // randomMovieIndex = getRndInteger(i+INTERVAL_BETWEEN_RANDOM, oldMovieIds.length);
+    // randomMovie = oldMovieIds.slice(i+INTERVAL_BETWEEN_RANDOM)[randomMovieIndex];
+    // newMovieIds.push(randomMovie);
+    // oldMovieIds = oldMovieIds.concat(movieIds.slice(i+INTERVAL_BETWEEN_RANDOM, randomMovieIndex)).concat(movieIds.slice(randomMovieIndex-1));
+    // console.log(i);
+    // i+=INTERVAL_BETWEEN_RANDOM;
+    // console.log(newMovieIds);
+    // console.log(oldMovieIds);
+  }
+  return movieIds;
 };
 
 export default movies;
