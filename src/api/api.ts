c7e1e39cd2ac5a87eb43a614224069cec641529a
johnnyloreano <laureano.pedrojoao@gmail.com>
2021-06-24 08:31:59 -0300

    DONE

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-const apiUrl = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api/v1";
-//const apiUrl = "http://localhost:5000/api/v1/";
+// const apiUrl = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api/v1";
+const apiUrl = "http://localhost:5000/api/v1/";
 
 const api = axios.create({
   baseURL: apiUrl,
